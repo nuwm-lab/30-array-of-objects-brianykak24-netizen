1d216ae3fed961067d6e9ba8d104792fe210d76e
brianykak24-netizen <brianyk_ak24@nuwm.edu.ua>
2025-11-20 01:07:53 +0200

    qwer

--- Program.cs
+++ Program.cs
@@ -7,7 +7,20 @@ namespace LabWork
         static void Main(string[] args)
         {
             int size = 0;
-            Console.Write("Enter size of array: "); size = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Enter size of array: "); size = Convert.ToInt32(Console.ReadLine());
+                if (size == 0)
+                {
+                    throw new Exception("Size of array cannot be zero.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
             VectorsList vectorsList = new VectorsList(size);
 
             Console.WriteLine("Vectors in the list:");
@@ -16,7 +29,7 @@ namespace LabWork
 
             Console.WriteLine("Vector with the largest length");
             NormalVector largestVector = vectorsList.GetVectorWithTheLargestLength();
-            if(largestVector != null) Console.WriteLine(largestVector.ToString());
+            if (largestVector != null) Console.WriteLine(largestVector.ToString());
 
 
         }
